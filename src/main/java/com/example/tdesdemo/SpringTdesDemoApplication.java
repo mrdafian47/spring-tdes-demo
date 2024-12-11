9701e6478df6aa840304b4260c8c60c4627c8ade
Mohamad Rizki <mohamad.rizki.dafianto@gmail.com>
2024-12-11 13:45:54 +0700

    init: initial commit

---
+++ src/main/java/com/example/tdesdemo/SpringTdesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTdesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTdesDemoApplication.class, args);
+	}
+
+}
